ffb9427d98f4085fac1f8807b58a0bd92f0f6d9a
dmzn <dmznlin@users.noreply.github.com>
2022-08-24 15:26:22 +0800

    +.添加循环队列circular,特性:FIFO或FILO,固定大小或自增.

---
+++ test/circular_test.go
@@ -0,0 +1,49 @@
+package test
+
+import (
+	"github.com/dmznlin/znlib-go/znlib"
+	"testing"
+	"unsafe"
+)
+
+func TestCircularInt(t *testing.T) {
+	queue := znlib.NewCircularQueue[int](znlib.Circular_FILO, 1)
+	queue.Push()                   //nil
+	queue.Push(10, 20, 30, 40, 50) //value list
+
+	num, cap := queue.Size()
+	t.Logf("size int: %d num:%d cap:%d", unsafe.Sizeof(queue), num, cap)
+	t.Logf("mult pop: %v", queue.MPop(3))
+
+	val, ok := queue.Pop(-1)
+	for ok {
+		t.Logf("pop val: %d", val)
+		val, ok = queue.Pop(-1)
+	}
+
+	queue.Push(1, 2, 3)
+	val, _ = queue.Pop(-1)
+	if val != 3 {
+		t.Error("znlib.CircularQueue.pop wrong")
+	}
+}
+
+func TestCircularString(t *testing.T) {
+	queue := znlib.NewCircularQueue[string](znlib.Circular_FIFO_FixSize, 10)
+	queue.Push("Hello", "World", "U", "Are", "Welcome") //value list
+
+	num, cap := queue.Size()
+	t.Logf("size string: %d num:%d cap:%d", unsafe.Sizeof(queue), num, cap)
+
+	val, ok := queue.Pop("a")
+	for ok {
+		t.Logf("pop val: %s", val)
+		val, ok = queue.Pop("a")
+	}
+
+	queue.Push("a", "b", "c")
+	val, _ = queue.Pop("")
+	if val != "a" {
+		t.Error("znlib.CircularQueue.pop wrong")
+	}
+}
---
+++ znlib/circular.go
@@ -0,0 +1,213 @@
+/*Package znlib ***************************************************************
+  作者: dmzn@163.com 2022-08-24 09:43:32
+  描述: 支持FIFO、FILO的循环队列
+
+  *.队列底层数据为双向链表,头尾闭合成环:
+	 <==> data <==> head <==> data <==> data <==> data
+	 ||											   ||
+	 |<== data <==>  data <==> tail <==> data <==> ||
+  添加数据时: next顺时针
+  读取数据时: FIFO为next顺时针,FILO为prior逆时针
+******************************************************************************/
+package znlib
+
+import (
+	"errors"
+	"sync"
+)
+
+//CircularMode 队列模式
+type CircularMode int8
+
+const (
+	Circular_FIFO         CircularMode = iota //队列模式: first in,first out
+	Circular_FIFO_FixSize                     //固定大小队列,旧数据被覆盖
+	Circular_FILO                             //栈模式: first in,last out
+	Circular_FILO_FixSize                     //固定大小栈,旧数据被覆盖
+)
+
+//circularItem 循环队列中的数据项
+type circularData[T any] struct {
+	data  T                //数据
+	prior *circularData[T] //前项
+	next  *circularData[T] //后项
+}
+
+//CircularQueue 循环队列
+type CircularQueue[T any] struct {
+	lock sync.RWMutex     //同步锁
+	mode CircularMode     //模式
+	cap  int              //队列容量
+	num  int              //数据个数
+	head *circularData[T] //队首数据
+	tail *circularData[T] //队尾数据
+}
+
+/*NewCircularQueue 2022-08-24 09:51:40
+  参数: mode,队列模式
+  参数: cap,初始化大小
+  描述: 生成一个 T 类型的环形队列
+
+  调用方法:
+  queue := NewCircularQueue[int](Circular_FIFO, 0)
+*/
+func NewCircularQueue[T any](mode CircularMode, size int) *CircularQueue[T] {
+	if mode > Circular_FILO_FixSize {
+		panic(errors.New("znlib.NewCircularQueue: invalid mode"))
+	}
+
+	if size < 1 {
+		size = 3 //循环链中,最少3各元素
+	}
+
+	queue := CircularQueue[T]{
+		tail: nil,
+		mode: mode,
+		cap:  size,
+		num:  0,
+	}
+
+	queue.head = &circularData[T]{prior: nil, next: nil}
+	cd := queue.head
+	//首元素
+
+	for i := 1; i < size; i++ {
+		cd.next = &circularData[T]{prior: cd, next: nil} //链尾新建元素
+		cd = cd.next
+	}
+
+	cd.next = queue.head
+	queue.head.prior = cd //闭合成环
+	return &queue
+}
+
+/*Push 2022-08-24 09:57:39
+  参数: values,值列表
+  描述: 添加一组值到队列中
+*/
+func (cq *CircularQueue[T]) Push(values ...T) {
+	if values == nil { //empty
+		return
+	}
+
+	cq.lock.Lock()
+	defer cq.lock.Unlock()
+
+	for _, val := range values {
+		if cq.tail == nil { //首次添加
+			cq.head.data = val
+			cq.tail = cq.head
+			cq.num++
+			continue
+		}
+
+		if cq.tail.next == cq.head { //尾部没有空间
+			if cq.mode == Circular_FIFO_FixSize || cq.mode == Circular_FILO_FixSize { //固定大小,覆盖旧数据
+				cq.head = cq.head.next
+				cq.tail = cq.tail.next
+				cq.tail.data = val
+			} else {
+				cq.tail.next = &circularData[T]{prior: cq.tail, next: cq.tail.next} //插入新元素
+				cq.tail = cq.tail.next
+				cq.tail.data = val
+
+				cq.cap++
+				cq.num++
+			}
+		} else { //尾部有空间
+			cq.tail = cq.tail.next
+			cq.tail.data = val
+			cq.num++
+		}
+	}
+}
+
+/*Pop 2022-08-24 12:06:12
+  参数: def,默认值
+  描述: 取出队列中的值,若不存在则返回默认
+*/
+func (cq *CircularQueue[T]) Pop(def T) (value T, ok bool) {
+	cq.lock.Lock()
+	defer cq.lock.Unlock()
+
+	ok = cq.tail != nil
+	if !ok { //队列为空
+		return def, ok
+	}
+
+	switch cq.mode {
+	case Circular_FIFO, Circular_FIFO_FixSize: //先进先出
+		value = cq.head.data
+		if cq.head == cq.tail { //最后一个元素
+			cq.tail = nil
+		} else {
+			cq.head = cq.head.next
+		}
+	case Circular_FILO, Circular_FILO_FixSize: //先进后出
+		value = cq.tail.data
+		if cq.head == cq.tail { //最后一个元素
+			cq.tail = nil
+		} else {
+			cq.tail = cq.tail.prior
+		}
+	}
+
+	cq.num--
+	return value, ok
+}
+
+/*MPop 2022-08-24 14:12:35
+  参数: num,个数
+  描述: 取出多个元素
+*/
+func (cq *CircularQueue[T]) MPop(num int) (values []T) {
+	cq.lock.Lock()
+	defer cq.lock.Unlock()
+
+	if cq.tail == nil || num <= 0 { //队列为空
+		return []T{}
+	}
+
+	var idx int = 0
+	values = make([]T, num)
+
+	for num > 0 {
+		switch cq.mode {
+		case Circular_FIFO, Circular_FIFO_FixSize: //先进先出
+			values[idx] = cq.head.data
+			idx++
+
+			if cq.head == cq.tail { //最后一个元素
+				cq.tail = nil
+			} else {
+				cq.head = cq.head.next
+			}
+		case Circular_FILO, Circular_FILO_FixSize: //先进后出
+			values[idx] = cq.tail.data
+			idx++
+
+			if cq.head == cq.tail { //最后一个元素
+				cq.tail = nil
+			} else {
+				cq.tail = cq.tail.prior
+			}
+		}
+
+		num--
+		cq.num-- //元素计数
+		if cq.tail == nil {
+			break
+		}
+	}
+
+	return values[:idx]
+}
+
+/*Size 2022-08-24 14:09:27
+  返回: num,有效元素个数
+  返回: cap,队列容量
+  描述: 返回队列容量和元素个数
+*/
+func (cq *CircularQueue[T]) Size() (num, cap int) {
+	return cq.num, cq.cap
+}
